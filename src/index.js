ffaaf5f799508a8efa75d80dee3eabdbcfbc544e
Angelica-zb <angelika.zaborovskaya@gmail.com>
2022-06-29 23:29:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let w = n.toString();
+    let d = w.length - 1;
+    let rez = '';
+    while (d >= 0) {
+        if (w[d] != '-') {
+            rez = rez + w[d]
+        } else { rez = rez }
+        d = d - 1;
+    }
+    return rez;
 }
